3615a5339b912f7455d53aa8366e13bca162fd61
fortune-max <fortmax120@gmail.com>
2023-03-19 21:35:15 +0100

    add navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import './globals.css'
 
 export const metadata = {
@@ -12,7 +13,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <nav className="nav">
+          <Link href="/">Home</Link>
+          <Link href="/favorites" prefetch={false}>Favorites</Link>
+        </nav>
+        {children}
+      </body>
     </html>
   )
 }
